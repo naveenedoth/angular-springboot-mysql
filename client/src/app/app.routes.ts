308087b10925a55d8f2a1cac5415a5834826c425
Naveen L S <naveenedoth@gmail.com>
2024-10-30 20:28:29 +0530

    Commit #4

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { SignupComponent } from './signup/signup.component'; 
-import { ProfileComponent } from './profile/profile.component';   
 
-export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirect to /login
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    { path: 'profile', component: ProfileComponent},
-    { path: '**', component: ErrorPageComponent }, // Wildcard route for unknown paths - this always at last
-];
+export const routes: Routes = [];
